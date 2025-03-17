ff61d2abdcfd3707bfab654010129a3ff956ea8d
Dayloc <158208881+Dayloc@users.noreply.github.com>
2025-03-17 21:31:40 +0000

    Creada la Lamding Page

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,62 @@
 import React from "react";
+import Card from "./card";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Footer from "./footer";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+// Datos dinámicos para las Cards
+const cardData = [
+  {
+    title: "Card 1",
+    description: "This is the first card.",
+    image: rigoImage,
+  },
+  {
+    title: "Card 2",
+    description: "This is the second card.",
+    image: rigoImage,
+  },
+  {
+    title: "Card 3",
+    description: "This is the third card.",
+    image: rigoImage,
+  },
+  {
+    title: "Card 4",
+    description: "This is the fourth card.",
+    image: rigoImage,
+  },
+];
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  return (
+    <div className="container-fluid p-0 text-center">
+      <Navbar />
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+      <Jumbotron />
+
+      <div className="container">
+        <div className="row">
+          {cardData.map((card, index) => (
+            <div key={index} className="col-md-3 col-sm-6 mb-4">
+              <Card
+                title={card.title}
+                description={card.description}
+                image={card.image}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/js/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      {" "}
+      <p>
+        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+        love!
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
